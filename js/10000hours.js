d09e6e58ea223bd09febefbf59ed4b896a00c5fa
hayeonii <hayexn51@gmail.com>
2022-11-03 01:56:53 +0900

    Feat : 훈련 시간 계산

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,26 @@
-// js file
+const btnHow = document.querySelector('#how-btn')
+
+btnHow.addEventListener('click', function(e) {
+    e.preventDefault()
+
+    // 값 받아와서 Result에 출력
+    const task = document.querySelector('#input-task').value
+    const hour = document.querySelector('#input-hour').value
+
+    const resultTask = document.querySelector('#result-task')
+    const resultHour = document.querySelector('#result-hour')
+
+    resultTask.innerHTML = task
+    resultHour.innerHTML = 10000/hour
+
+    
+    // 시계 효과, 결과 보여주기
+    const clock = document.querySelector('.clock')
+    const result = document.querySelector('.contents-result')
+    clock.style.display = 'block'
+
+    setTimeout(() => {
+        clock.style.display = 'none'
+        result.style.display = 'block'
+    }, 3000)
+})
