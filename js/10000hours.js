b32111aafaae18810bd0a8a7d30231834600914b
hayeonii <hayexn51@gmail.com>
2022-11-04 13:25:06 +0900

    feat : 예외처리

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,33 +1,36 @@
 const btnHow = document.querySelector('#how-btn')
+const goTraining = document.querySelector('#training')
+const realTraining = document.querySelector('#real-training')
+const share = document.querySelector('#share')
 
 btnHow.addEventListener('click', function(e) {
     e.preventDefault()
 
-    // 값 받아와서 Result에 출력
-    const task = document.querySelector('#input-task').value
-    const hour = document.querySelector('#input-hour').value
-
+    const task = document.querySelector('#input-task')
+    const hour = document.querySelector('#input-hour')
     const resultTask = document.querySelector('#result-task')
     const resultHour = document.querySelector('#result-hour')
-
-    resultTask.innerHTML = task
-    resultHour.innerHTML = Math.floor(10000/hour)
-
-    
-    // 시계 효과, 결과 보여주기
     const clock = document.querySelector('.clock')
     const result = document.querySelector('.contents-result')
 
-    clock.style.display = 'block'
+    
+
+    if (task.value === '' || hour.value === '') {
+        alert('빈 곳을 모두 입력해 주세요')
+        return (result.style.display = 'none')
+    }
+
     result.style.display = 'none'
-    setTimeout(() => {
+    clock.style.display = 'block'
+    
+    setTimeout((e) => {
         clock.style.display = 'none'
         result.style.display = 'block'
+        resultTask.innerHTML = task.value
+        resultHour.innerHTML = Math.floor(10000/hour.value)
     }, 3000)
 })
 
-
-const goTraining = document.querySelector('#training')
 
 // modal 띄우기
 goTraining.addEventListener('click', function() {
@@ -35,7 +38,6 @@ goTraining.addEventListener('click', function() {
     modal.style.display = 'block'
 })
 
-const realTraining = document.querySelector('#real-training')
 
 // modal 닫기 (나중에 닫기 버튼 따로 만들고 링크 연결하면 좋을 듯)
 realTraining.addEventListener('click', function(e) {
@@ -44,7 +46,6 @@ realTraining.addEventListener('click', function(e) {
     e.preventDefault()
 })
 
-const share = document.querySelector('#share')
 
 // URL 복사
 share.addEventListener('click', function() {
